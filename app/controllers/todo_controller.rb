1c7c0f1bca5d50762b5072546bc3a21b1bc9bf49
Maximo Bahamondes <maximoabahamondes@gmail.com>
2017-06-17 19:03:06 +0000

    addded ruby variables

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,5 +2,8 @@ class TodoController < ApplicationController
     def index
     end
     def show
+        @todo_description = "Make a curriculum"
+        @todo_pomodoro_estamate = 4
     end
 end
+
