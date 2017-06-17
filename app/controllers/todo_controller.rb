e38c12303c76eb874de7d3311872ef6f1ddd795f
Maximo Bahamondes <maximoabahamondes@gmail.com>
2017-06-17 21:43:30 +0000

    multiple appendages past todo/show/

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,8 +2,19 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        @todo_description = "Make a curriculum"
-        @todo_pomodoro_estamate = 4
+        todo_id = params[:id]
+        # todo_id = '0'
+            @todo_description = "Make 1 curriculum"
+            @todo_pomodoro_estamate = 4
+        if todo_id == '1' 
+            @todo_description = "Clean my room"
+            @todo_pomodoro_estamate = 3
+        elsif todo_id == '2' 
+            @todo_description = "Get food"
+            @todo_pomodoro_estamate = 1
+        elsif todo_id == '3' 
+            @todo_description = "Go to school"
+            @todo_pomodoro_estamate = 14
+        end
     end
 end
-
